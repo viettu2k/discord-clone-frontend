93bc6ca675535f7500ee40f803a0b94088177d42
viettu2k <doanviettu2k@gmail.com>
2022-02-25 17:07:49 +0700

    creating custom input component

--- src/authPage/LoginPage/LoginPage.jsx
+++ src/authPage/LoginPage/LoginPage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginPageHeader from "./LoginPageHeader";
+import LoginPageInputs from "./LoginPageInputs";
 
 export default function LoginPage() {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <AuthBox>
       <LoginPageHeader />
+      <LoginPageInputs
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+      />
     </AuthBox>
   );
 }
