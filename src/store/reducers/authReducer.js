a56d2882182722dfa4f2ca7c60aaccf0f0869177
viettu2k <doanviettu2k@gmail.com>
2022-02-26 15:03:32 +0700

    preparing authentication actions

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,12 +1,15 @@
+import { authActions } from "../actions/authActions";
+
 const initialState = {
     userDetails: null,
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "DUMMY":
+        case authActions.SET_USER_DETAILS:
             return {
                 ...state,
+                userDetails: action.userDetails,
             };
         default:
             return state;
