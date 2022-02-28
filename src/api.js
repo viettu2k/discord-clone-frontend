01cff5eaed01f0d46dbc9ddf26742a84c49753fa
viettu2k <doanviettu2k@gmail.com>
2022-02-28 09:27:39 +0700

    adding JWT token to request

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
 import axios from "axios";
+import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
     baseURL: "http://localhost:5002/api",
     timeout: 1000,
 });
+
+apiClient.interceptors.request.use((config) => {
+    const userDetails = localStorage.getItem("user");
+
+    if (userDetails) {
+        const token = JSON.parse(userDetails).token;
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+});
+
+// public routers
 
 export const login = async(data) => {
     try {
@@ -24,5 +36,15 @@ export const register = async(data) => {
             error: true,
             exception,
         };
+    }
+};
+
+// secure routes
+
+const checkResponseCode = (exception) => {
+    const responseCode = exception.response.status;
+
+    if (responseCode) {
+        (responseCode === 401 || responseCode === 403) && logout();
     }
 };
