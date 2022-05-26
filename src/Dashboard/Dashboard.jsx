9b7b8a86cf929c4a0016386d981c92f5e1404cac
viettu2k <doanviettu2k@gmail.com>
2022-05-26 15:37:02 +0700

    creating dashboard stucture

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react';
+import { styled } from '@mui/system';
+import SideBar from './SideBar/SideBar';
+import FriendSideBar from './FriendSideBar/FriendSideBar';
+import Messenger from './Messenger/Messenger';
+import AppBar from './AppBar/AppBar';
+
+const Wrapper = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+});
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Wrapper>
+      <SideBar />
+      <FriendSideBar />
+      <Messenger />
+      <AppBar />
+    </Wrapper>
+  );
 }
