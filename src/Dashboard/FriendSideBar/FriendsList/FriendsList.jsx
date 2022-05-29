d2ba78a2e41724e6492b164ea1fafdbd54c15bfe
viettu2k <doanviettu2k@gmail.com>
2022-05-29 10:25:56 +0700

    preparing friends list

--- src/Dashboard/FriendSideBar/FriendsList/FriendsList.jsx
+++ src/Dashboard/FriendSideBar/FriendsList/FriendsList.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { styled } from '@mui/system';
+import FriendsListItem from './FriendsListItem';
+
+const DUMMY_FRIEND = [
+  { id: 1, username: 'Mark', isOnline: true },
+  { id: 2, username: 'Dark', isOnline: false },
+  { id: 3, username: 'Bruh', isOnline: true },
+  { id: 4, username: 'Lmao', isOnline: false },
+];
 
 const MainContainer = styled('div')({
   flexGrow: 1,
@@ -7,7 +15,17 @@ const MainContainer = styled('div')({
 });
 
 const FriendsList = () => {
-  return <MainContainer>FriendsList</MainContainer>;
+  return (
+    <MainContainer>
+      {DUMMY_FRIEND.map((friend) => (
+        <FriendsListItem
+          key={friend.id}
+          username={friend.username}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </MainContainer>
+  );
 };
 
 export default FriendsList;
