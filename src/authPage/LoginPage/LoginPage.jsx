4ce9f447c18f821a7b662930e93fa5c1b6674866
viettu2k <doanviettu2k@gmail.com>
2022-06-02 10:11:36 +0700

    updating auth pages

--- src/authPage/LoginPage/LoginPage.jsx
+++ src/authPage/LoginPage/LoginPage.jsx
@@ -1,23 +1,30 @@
-import React, { useState, useEffect } from "react";
-import AuthBox from "../../shared/components/AuthBox";
-import { validateLoginForm } from "../../shared/utils/validators";
-import LoginPageFooter from "./LoginPageFooter";
-import LoginPageHeader from "./LoginPageHeader";
-import LoginPageInputs from "./LoginPageInputs";
-import { connect } from "react-redux";
-import { getActions } from "../../store/actions/authActions";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import AuthBox from '../../shared/components/AuthBox';
+import { validateLoginForm } from '../../shared/utils/validators';
+import LoginPageFooter from './LoginPageFooter';
+import LoginPageHeader from './LoginPageHeader';
+import LoginPageInputs from './LoginPageInputs';
+import { connect } from 'react-redux';
+import { getActions } from '../../store/actions/authActions';
+import { useNavigate } from 'react-router-dom';
 
 export function LoginPage({ login }) {
   const history = useNavigate();
 
-  const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
     setIsFormValid(validateLoginForm({ mail, password }));
   }, [mail, password, setIsFormValid]);
+
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user');
+    if (userDetails) {
+      window.location.pathname = 'dashboard';
+    }
+  }, []);
 
   const handleLogin = () => {
     const userDetails = {
--- src/authPage/RegisterPage/RegisterPage.jsx
+++ src/authPage/RegisterPage/RegisterPage.jsx
@@ -1,25 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
-import AuthBox from "../../shared/components/AuthBox";
-import { validateRegisterForm } from "../../shared/utils/validators";
-import RegisterPageFooter from "./RegisterPageFooter";
-import RegisterPageInputs from "./RegisterPageInputs";
-import { connect } from "react-redux";
-import { getActions } from "../../store/actions/authActions";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@mui/material';
+import AuthBox from '../../shared/components/AuthBox';
+import { validateRegisterForm } from '../../shared/utils/validators';
+import RegisterPageFooter from './RegisterPageFooter';
+import RegisterPageInputs from './RegisterPageInputs';
+import { connect } from 'react-redux';
+import { getActions } from '../../store/actions/authActions';
+import { useNavigate } from 'react-router-dom';
 
 export function RegisterPage({ register }) {
   const history = useNavigate();
 
-  const [mail, setMail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [mail, setMail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
     setIsFormValid(validateRegisterForm({ mail, username, password }));
   }, [mail, username, password, setIsFormValid]);
+
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user');
+    if (userDetails) {
+      window.location.pathname = 'dashboard';
+    }
+  }, []);
 
   const handleRegister = () => {
     const userDetails = {
@@ -33,7 +40,7 @@ export function RegisterPage({ register }) {
 
   return (
     <AuthBox>
-      <Typography variant="h5" sx={{ color: "white" }}>
+      <Typography variant="h5" sx={{ color: 'white' }}>
         Create an account
       </Typography>
       <RegisterPageInputs
