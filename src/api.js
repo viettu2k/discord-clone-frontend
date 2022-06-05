017bc3dcaa39c5c7ebb4715997952d37cfd5a8d3
viettu2k <doanviettu2k@gmail.com>
2022-06-05 21:13:03 +0700

    preparing  action to send friend invitation

--- src/api.js
+++ src/api.js
@@ -2,56 +2,67 @@ import axios from 'axios';
 import { logout } from './shared/utils/auth';
 
 const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    timeout: 1000,
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 1000,
 });
 
 apiClient.interceptors.request.use(
-    (config) => {
-        const userDetails = localStorage.getItem('user');
+  (config) => {
+    const userDetails = localStorage.getItem('user');
 
-        if (userDetails) {
-            const token = JSON.parse(userDetails).token;
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // public routers
 
-export const login = async(data) => {
-    try {
-        return await apiClient.post('/auth/login', data);
-    } catch (exception) {
-        return {
-            error: true,
-            exception,
-        };
-    }
+export const login = async (data) => {
+  try {
+    return await apiClient.post('/auth/login', data);
+  } catch (exception) {
+    return {
+      error: true,
+      exception,
+    };
+  }
 };
 
-export const register = async(data) => {
-    try {
-        return await apiClient.post('/auth/register', data);
-    } catch (exception) {
-        return {
-            error: true,
-            exception,
-        };
-    }
+export const register = async (data) => {
+  try {
+    return await apiClient.post('/auth/register', data);
+  } catch (exception) {
+    return {
+      error: true,
+      exception,
+    };
+  }
 };
 
 // secure routes
+export const sendFriendInvitation = async (data) => {
+  try {
+    return await apiClient.post('/friend-invitation/invite', data);
+  } catch (exception) {
+    checkResponseCode(exception);
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
 
 const checkResponseCode = (exception) => {
-    const responseCode = exception.response.status;
+  const responseCode = exception?.response?.status;
 
-    if (responseCode) {
-        (responseCode === 401 || responseCode === 403) && logout();
-    }
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
 };
