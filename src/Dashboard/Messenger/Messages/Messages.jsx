8e53b49db5f40a7f4c3ec71e6bcfe54314660a79
viettu2k <doanviettu2k@gmail.com>
2022-06-17 21:25:57 +0700

    creating messenger header

--- src/Dashboard/Messenger/Messages/Messages.jsx
+++ src/Dashboard/Messenger/Messages/Messages.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { styled } from '@mui/system';
+import MessagesHeader from './MessagesHeader';
+import { connect } from 'react-redux';
 
-const Messages = () => {
-  return <div>Messages</div>;
+const MainContainer = styled('div')({
+  height: 'calc(100% - 60px)',
+  overflowY: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Messages = ({ chosenChatDetails, messages }) => {
+  return (
+    <MainContainer>
+      <MessagesHeader name={chosenChatDetails?.name} />
+    </MainContainer>
+  );
 };
 
-export default Messages;
+const mapStoreStateToProps = ({ chat }) => {
+  return { ...chat };
+};
+
+export default connect(mapStoreStateToProps)(Messages);
