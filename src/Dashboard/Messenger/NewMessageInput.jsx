aa11202a6817ca83093909911f6b153f585ab5a6
viettu2k <doanviettu2k@gmail.com>
2022-06-20 23:18:22 +0700

    creating new message input

--- src/Dashboard/Messenger/NewMessageInput.jsx
+++ src/Dashboard/Messenger/NewMessageInput.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { styled } from '@mui/system';
 
-const NewMessageInput = () => {
-  return <div></div>;
+const MainContainer = styled('div')({
+  height: '60px',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const Input = styled('input')({
+  backgroundColor: '#2f3136',
+  height: '98%',
+  color: 'white',
+  border: 'none',
+  borderRadius: '8px',
+  fontSize: '14px',
+  padding: '0 10px',
+});
+
+const NewMessageInput = ({ chosenChatDetails }) => {
+  const [message, setMessage] = useState('');
+
+  const handleMessageValueChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    console.log('send message');
+    setMessage('');
+  };
+
+  const handleKeyPressed = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleSendMessage();
+    }
+  };
+
+  return (
+    <MainContainer>
+      <Input
+        placeholder={`Write message to ${chosenChatDetails.name}`}
+        value={message}
+        onChange={handleMessageValueChange}
+        onKeyDown={handleKeyPressed}
+      />
+    </MainContainer>
+  );
 };
 
-export default NewMessageInput;
+const mapStoreStateToProps = ({ chat }) => {
+  return { ...chat };
+};
+
+export default connect(mapStoreStateToProps)(NewMessageInput);
