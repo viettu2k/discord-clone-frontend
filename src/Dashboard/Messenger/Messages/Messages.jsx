b76eaf4ca9d2a9ccd39a0cba7b438d5996feab3d
viettu2k <doanviettu2k@gmail.com>
2022-06-23 15:49:18 +0700

    rendering real messages

--- src/Dashboard/Messenger/Messages/Messages.jsx
+++ src/Dashboard/Messenger/Messages/Messages.jsx
@@ -13,20 +13,51 @@ const MainContainer = styled('div')({
   alignItems: 'center',
 });
 
+const convertDateToHumanReadable = (date, format) => {
+  const map = {
+    mm: date.getMonth() + 1,
+    dd: date.getDate(),
+    yy: date.getFullYear().toString().slice(-2),
+    yyyy: date.getFullYear(),
+  };
+
+  return format.replace(/mm|dd|yyyy|yy/gi, (matched) => map[matched]);
+};
+
 const Messages = ({ chosenChatDetails, messages }) => {
   return (
     <MainContainer>
       <MessagesHeader name={chosenChatDetails?.name} />
-      {DUMMY_MESSAGES.map((message, index) => (
-        <Message
-          key={message._id}
-          content={message.content}
-          username={message.username}
-          sameAuthor={message.sameAuthor}
-          date={message.date}
-          sameDay={message.sameDay}
-        />
-      ))}
+      {messages.map((message, index) => {
+        const sameAuthor =
+          index > 0 &&
+          messages[index].author._id === messages[index - 1].author._id;
+
+        const sameDay =
+          index > 0 &&
+          convertDateToHumanReadable(new Date(message.date), 'dd/mm/yy') ===
+            convertDateToHumanReadable(
+              new Date(messages[index - 1].date),
+              'dd/mm/yy'
+            );
+
+        console.log(message.date);
+        console.log(convertDateToHumanReadable(message.date, 'dd/mm/yy'));
+
+        return (
+          <Message
+            key={message._id}
+            content={message.content}
+            username={message.author.username}
+            sameAuthor={sameAuthor}
+            date={convertDateToHumanReadable(
+              new Date(message.date),
+              'dd/mm/yy'
+            )}
+            sameDay={sameDay}
+          />
+        );
+      })}
     </MainContainer>
   );
 };
