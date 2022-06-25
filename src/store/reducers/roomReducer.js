9502c1b055da900e94b70c2cec32bff1c85afd6f
viettu2k <doanviettu2k@gmail.com>
2022-06-25 16:26:17 +0700

    preparing store state

---
+++ src/store/reducers/roomReducer.js
@@ -0,0 +1,19 @@
+const initState = {
+    isUserInRoom: false,
+    isUserRoomCreator: false,
+    roomDetails: null,
+    activeRooms: [],
+    localStream: null,
+    remoteStreams: [],
+    audioOnly: false,
+    screenSharingStream: null,
+    isScreenSharingActive: false,
+};
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+};
+
+export default reducer;
